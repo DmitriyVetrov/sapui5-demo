38b6d132509eb3d694038acc6dfbff0141663b47
Dmytro Vietrov <dmitriy.vetrov@gmail.com>
2026-03-31 13:21:49 +0000

    Initial Commit @3/31/2026, 1:21:48 PM

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["vietrov/projectvietrov/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"vietrov/projectvietrov/test/unit/controller/ViewMain.controller"
+], function () {
+	"use strict";
+});
